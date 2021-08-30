a68f19a75d0e5a0d115e45dc4e4dee4b7177ffa9
ShinyDialga <shinydialga45@gmail.com>
2021-08-30 14:15:36 -0500

    Add config for quit messages

--- core/src/main/java/tc/oc/pgm/PGMConfig.java
+++ core/src/main/java/tc/oc/pgm/PGMConfig.java
@@ -92,6 +92,7 @@ public final class PGMConfig implements Config {
   private final boolean statsShowBest;
   private final boolean statsShowOwn;
   private final int verboseItemSlot;
+  private final boolean quitMessages;
 
   // sidebar.*
   private final Component header;
@@ -193,6 +194,8 @@ public final class PGMConfig implements Config {
     this.statsShowBest = parseBoolean(config.getString("stats.show-best", "true"));
     this.statsShowOwn = parseBoolean(config.getString("stats.show-own", "true"));
     this.verboseItemSlot = parseInteger(config.getString("stats.item-slot", "7"));
+
+    this.quitMessages = parseBoolean(config.getString("ui.quit-messages", "false"));
 
     final String header = config.getString("sidebar.header");
     this.header = header == null || header.isEmpty() ? null : parseComponent(header);
@@ -613,6 +616,11 @@ public final class PGMConfig implements Config {
   @Override
   public int getVerboseItemSlot() {
     return verboseItemSlot;
+  }
+
+  @Override
+  public boolean showQuitMessages() {
+    return quitMessages;
   }
 
   @Override
